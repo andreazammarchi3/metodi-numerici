f2c4253bdb535dee58f2870ca2b07d6c59166e3a
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2022-03-09 18:17:44 +0100

    Add es02

--- esercitazioni/es02/es02_02.m
+++ esercitazioni/es02/es02_02.m
@@ -1,1 +1,11 @@
+clc
+clear all
+
+format rat
+
+x=2^52;     % x = 4503599627370496
+x=x+1      % x+1 = 4503599627370497
+y=x;
+y=y+0.5     % x+0.5 = 4503599627370497.5
+            % viene arrotondato a ...98
 
