4d68eb730b05d3df5859780987fd7a4b853f22b2
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2022-04-28 14:43:02 +0200

    Update prova1.m

--- Modulo 2/Es. 01 - marzo 29/Soluzioni/prova1.m
+++ Modulo 2/Es. 01 - marzo 29/Soluzioni/prova1.m
@@ -10,12 +10,50 @@ n1Ainv=zeros(50,1);
 
 k=1;
 
-for alpha=linspace(0.001,2/3,50)
+for alpha=linspace(0.001, 2/3, 50)
     n1A(k)=my_norm_1(A(alpha));
-    n1Ainv(k)=my_norm_1(A(alpha));
+    n1Ainv(k)=my_norm_1(Ainv(alpha));
+    k=k+1;
 end
-condA=n1A.*n1Ainv;
 
-disp(n1A);
-disp(n1Ainv);
-disp(condA);
+disp("n1A=" + n1A);
+disp("naAinv=" + n1Ainv);
+% a) Norma 1 di A =1+3*a, ha valore maggiore quando a=2/3, quindi = 3
+
+% b) Norma 1 di inv(A) =2/a, ha valore maggiore con a=0.001, quindi = 2000
+
+
+condA=n1A.*n1Ainv;
+alpha=linspace(0.001, 2/3, 50);
+figure
+plot(alpha, condA,'b')
+% c) Il numero di condizionamento diminuisce esponenzialmente con
+% l'aumentare di alpha.
+
+% d) Il miglior condizionamento su A si ha quando alpha =2/3
+
+% e) Il peggior condizionamento su A si ha quando alpha =0.001
+
+
+b=[-8; -1; 9];
+delta=[0.01 0.001 0.001; 0 0.001 0.001; 0 0.001 0.01];
+k=1;
+
+for alpha=linspace(0.001, 2/3, 50)
+    matA=A(alpha);
+    x=matA\b;
+    matApert=matA+delta;
+    xpert=matApert\b;
+    err_perc_dat(k)=my_norm_1(delta)/my_norm_1(matA)*100;
+    err_perc_sol(k)=my_norm_1(xpert-x)/my_norm_1(x)*100;
+    k=k+1;
+end
+
+Erel_perc_dat=err_perc_dat';
+Erel_perc_sol=err_perc_sol';
+
+T=table(Erel_perc_dat, Erel_perc_sol)
+
+
+
+
